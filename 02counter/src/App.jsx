48d2b8c892f34f097598eb0cb5f23009c47e7d7d
VIVEK SINGH <77235217+viveksingh31722@users.noreply.github.com>
2025-01-22 01:24:31 +0530

    Learned about basic structure and file management of react, and also learned to write a custom react library, At last created a basic project "Counter" using Hook called "useState" for state management in ui.

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(0)
+
+  // let counter = 15
+  
+  const addValue = () =>{
+     
+   // counter = counter + 1
+   // pass the above counter to setCounter or 
+   if(counter < 20)
+   setCounter(counter + 1);
+  };
+
+  const decreaseValue = () =>{
+    
+    if(counter > 0){
+      setCounter(counter - 1);
+    }
+    
+  }
+
+  return (
+    <>
+     <h1>Counter project</h1>
+     <h2>Counter Value: {counter}</h2>
+
+     <button onClick={addValue}>Add value {counter}</button>
+     <br />
+     <button onClick={decreaseValue}>Decrease value {counter}</button>
+     <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
 import App from './App.jsx'
+
+function MyApp(){
+  const userName = 'with vivek singh'
+  return(
+    <>
+      <h1>Custom App {userName}</h1> {/*This syntax {userName} is called evaluated expression*/}
+    </>
+  )
+}
+
+//This is simply creating a variable and passing it ot root.render function
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const anotherUser = 'with vivek'
+
+//This is the react pre-defined syntax for creating a reactElement
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherUser
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <MyApp />
   </StrictMode>,
+  // anotherElement
+  // reactElement
+  
 )
